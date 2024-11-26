c61a8ebb3b144f9bdf64cf797fa6c85484c49850
V-inTim <vinichuk.timofey@mail.ru>
2024-11-27 00:57:18 +0300

    Add annotation Adult.

---
+++ calculator/src/main/java/com/example/calculator/validator/Adult.java
@@ -0,0 +1,18 @@
+package com.example.calculator.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = AdultValidator.class)
+public  @interface Adult {
+    String message() default "Must be at least 18 years old";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ calculator/src/main/java/com/example/calculator/validator/AdultValidator.java
@@ -0,0 +1,21 @@
+package com.example.calculator.validator;
+
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+
+public class AdultValidator implements ConstraintValidator<Adult, LocalDate> {
+    @Override
+    public boolean isValid(LocalDate birthdate, ConstraintValidatorContext constraintValidatorContext) {
+        if (birthdate == null) {
+            return true; // Исходя из логики, что, если нужно проверить на Null, используй @NotNull
+        }
+        LocalDate now = LocalDate.now();
+        Period age = Period.between(birthdate, now);
+        return age.getYears() >= 18;
+    }
+}
