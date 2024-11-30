c4ea41c3d1c91f1fecbdba407414416f5a9f14ea
V-inTim <vinichuk.timofey@mail.ru>
2024-11-30 17:33:22 +0300

    Add calculatePaymentSchedule in CalculatorService.

--- calculator/src/main/java/com/example/calculator/service/CalculatorService.java
+++ calculator/src/main/java/com/example/calculator/service/CalculatorService.java
@@ -1,9 +1,13 @@
 package com.example.calculator.service;
 
+import com.example.calculator.dto.PaymentScheduleElementDto;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CalculatorService {
@@ -25,5 +29,34 @@ public class CalculatorService {
         BigDecimal numerator = x.multiply(rate).multiply(amount);
         BigDecimal denominator = x.subtract(one);
         return numerator.divide(denominator, 2, RoundingMode.HALF_UP);
+    }
+
+    public BigDecimal calculatePsk(BigDecimal monthlyPayment, int termInMonths) {
+        return monthlyPayment.multiply(new BigDecimal(termInMonths));
+    }
+
+    public List<PaymentScheduleElementDto> calculatePaymentSchedule(
+            BigDecimal totalPayment, BigDecimal rate, BigDecimal psk, int term) {
+        List<PaymentScheduleElementDto> schedule = new ArrayList<>();
+        BigDecimal interestPayment, debtPayment, balance = psk;
+
+        for (int i=1; i < term + 1; i++){
+
+            interestPayment = balance.add(rate);
+            debtPayment = totalPayment.subtract(interestPayment);
+            balance = balance.subtract(totalPayment);
+            LocalDate date = LocalDate.now().plusMonths(i);
+
+            schedule.add(
+                    PaymentScheduleElementDto.builder()
+                            .number(i)
+                            .date(date)
+                            .totalPayment(totalPayment)
+                            .interestPayment(interestPayment)
+                            .debtPayment(debtPayment)
+                            .remainingDebt(balance)
+                            .build());
+        }
+        return  schedule;
     }
 }
