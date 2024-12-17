25e915f3af74126ccd3c7c4e397abcaac717ba57
V-inTim <vinichuk.timofey@mail.ru>
2024-12-17 18:06:09 +0300

    Add DealHandler.

---
+++ deal/src/main/java/com/example/deal/handler/DealHandler.java
@@ -0,0 +1,37 @@
+package com.example.deal.handler;
+
+import com.example.deal.exception.CalculatorErrorException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class DealHandler  {
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidFormatException(InvalidFormatException ex) {
+        Map<String, String> errors = new HashMap<>();
+        String fieldName = ex.getPath().get(0).getFieldName();
+        String errorMessage = String.format("Invalid format for field: %s", ex.getValue());
+        errors.put(fieldName, errorMessage);
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleNotValidException(MethodArgumentNotValidException ex, WebRequest request){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+    @ExceptionHandler(CalculatorErrorException.class)
+    public ResponseEntity<Map<String, Object>> handleNotValidException(CalculatorErrorException ex, WebRequest request){
+        return new ResponseEntity<>(ex.getResponseBody(), ex.getStatusCode());
+    }
+}
