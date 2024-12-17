a462f07b54f64cafd49f2c8cd04d18a3e84d7b0f
V-inTim <vinichuk.timofey@mail.ru>
2024-12-17 18:05:33 +0300

    Add CalculatorErrorException.

---
+++ deal/src/main/java/com/example/deal/exception/CalculatorErrorException.java
@@ -0,0 +1,21 @@
+package com.example.deal.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+import java.util.Map;
+
+@Getter
+public class CalculatorErrorException extends RuntimeException{
+    private HttpStatusCode statusCode;
+    private Map<String, Object> responseBody;
+    public CalculatorErrorException(HttpStatusCode statusCode, Map<String, Object> responseBody) {
+        super("HTTP Error: " + statusCode.toString());
+        this.statusCode = statusCode;
+        this.responseBody = responseBody;
+    }
+
+}
+
+
