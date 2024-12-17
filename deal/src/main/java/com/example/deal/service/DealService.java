6cb0023cb2562348d4da363bc349234034ad60c2
V-inTim <vinichuk.timofey@mail.ru>
2024-12-17 23:26:12 +0300

    Add applyOffer to DealService.

--- deal/src/main/java/com/example/deal/service/DealService.java
+++ deal/src/main/java/com/example/deal/service/DealService.java
@@ -5,15 +5,23 @@ import com.example.deal.dto.LoanOfferDto;
 import com.example.deal.dto.LoanStatementRequestDto;
 import com.example.deal.entity.Client;
 import com.example.deal.entity.Statement;
+import com.example.deal.entity.StatusHistory;
+import com.example.deal.exception.DbException;
 import com.example.deal.mapper.ClientMapper;
+import com.example.deal.mapper.OfferMapper;
 import com.example.deal.repository.ClientRepository;
 import com.example.deal.repository.StatementRepository;
 import com.example.deal.type.ApplicationStatus;
+import com.example.deal.type.ChangeType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 @Service
@@ -21,16 +29,19 @@ public class DealService {
     private final ClientRepository clientRepository;
     private final StatementRepository statementRepository;
     private final ClientMapper clientMapper;
+    private final OfferMapper offerMapper;
     private final CalculatorClient calculatorClient;
 
     @Autowired
     public DealService(ClientRepository clientRepository,
                        StatementRepository statementRepository,
                        ClientMapper clientMapper,
+                       OfferMapper offerMapper,
                        CalculatorClient calculatorClient) {
 
         this.clientRepository = clientRepository;
         this.clientMapper = clientMapper;
+        this.offerMapper = offerMapper;
         this.statementRepository = statementRepository;
         this.calculatorClient = calculatorClient;
     }
@@ -50,6 +61,30 @@ public class DealService {
         offers.forEach(offer -> offer.setStatementId(statement.getStatementId()));
 
         return offers;
+    }
+
+    public void applyOffer(LoanOfferDto dto) throws DbException{
+        UUID id = dto.getStatementId();
+        // находим, если есть
+        Optional<Statement> optionalStatement = statementRepository.findById(id);
+        if (optionalStatement.isEmpty())
+            throw new DbException("Resource with the given ID does not exist.", HttpStatus.BAD_REQUEST);
+        Statement statement = optionalStatement.get();
+        // заполняем
+        statement.setStatus(ApplicationStatus.APPROVED);
+
+        List<StatusHistory> history = statement.getStatusHistory();
+        if (history == null)
+            history = new ArrayList<StatusHistory>();
+        history.add(StatusHistory.builder()
+                .status(ApplicationStatus.APPROVED)
+                .changeType(ChangeType.AUTOMATIC)
+                .time(LocalDateTime.now()).build());
+        statement.setStatusHistory(history);
+
+        statement.setAppliedOffer(offerMapper.dtoToAppliedOffer(dto));
+        // сохраняем изменения
+        statementRepository.save(statement);
     }
 
 }
