253d7e9ab9e39efd16d9664abfac0c0dda255eb9
V-inTim <vinichuk.timofey@mail.ru>
2025-01-13 01:44:24 +0300

    Change DealService (add kafka logic).

--- deal/src/main/java/com/example/deal/service/DealService.java
+++ deal/src/main/java/com/example/deal/service/DealService.java
@@ -3,57 +3,60 @@ package com.example.deal.service;
 import com.example.deal.client.CalculatorClient;
 import com.example.deal.dto.*;
 import com.example.deal.entity.*;
-import com.example.deal.exception.DbException;
 import com.example.deal.mapper.ClientMapper;
 import com.example.deal.mapper.CreditMapper;
 import com.example.deal.mapper.OfferMapper;
 import com.example.deal.repository.ClientRepository;
 import com.example.deal.repository.CreditRepository;
-import com.example.deal.repository.StatementRepository;
 import com.example.deal.type.ApplicationStatus;
-import com.example.deal.type.ChangeType;
 import com.example.deal.type.CreditStatus;
+import com.example.deal.type.Topic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 
 @Service
 public class DealService {
     private final ClientRepository clientRepository;
-    private final StatementRepository statementRepository;
     private final CreditRepository creditRepository;
+    private final StatementService statementService;
     private final ClientMapper clientMapper;
     private final OfferMapper offerMapper;
     private final CreditMapper creditMapper;
     private final CalculatorClient calculatorClient;
+    private  final KafkaProducerService producerService;
+
+    @Value("${deal.document.url}")
+    private String url;
 
     private static final Logger logger = LoggerFactory.getLogger(DealService.class);
 
     @Autowired
     public DealService(ClientRepository clientRepository,
-                       StatementRepository statementRepository,
                        CreditRepository creditRepository,
+                       StatementService statementService,
                        ClientMapper clientMapper,
                        OfferMapper offerMapper,
                        CreditMapper creditMapper,
-                       CalculatorClient calculatorClient) {
+                       CalculatorClient calculatorClient,
+                       KafkaProducerService producerService) {
 
         this.clientRepository = clientRepository;
         this.creditRepository = creditRepository;
-        this.statementRepository = statementRepository;
+        this.statementService = statementService;
         this.clientMapper = clientMapper;
         this.offerMapper = offerMapper;
         this.creditMapper = creditMapper;
         this.calculatorClient = calculatorClient;
+        this.producerService = producerService;
     }
 
     public List<LoanOfferDto> createStatement(LoanStatementRequestDto requestData){
@@ -66,8 +69,9 @@ public class DealService {
                 .creationDate(LocalDateTime.now())
                 .clientId(client)
                 .status(ApplicationStatus.PREAPPROVAL)
+                .statusHistory(new ArrayList<>())
                 .build();
-        statementRepository.save(statement);
+        statementService.saveStatement(statement);
         logger.debug("createStatement, save statement");
 
         List<LoanOfferDto> offers = calculatorClient.requestOffers(requestData);
@@ -76,48 +80,30 @@ public class DealService {
         return offers;
     }
 
-    public void applyOffer(LoanOfferDto dto) throws DbException{
-        UUID id = dto.getStatementId();
-        // находим, если есть
-        Optional<Statement> optionalStatement = statementRepository.findById(id);
-        if (optionalStatement.isEmpty()){
-            logger.debug("applyOffer, statement DbException");
-            throw new DbException("Ресурс с данным id не сущетсвует.", HttpStatus.BAD_REQUEST);
-        }
-        logger.debug("applyOffer, find statement");
-        Statement statement = optionalStatement.get();
-        // заполняем
-        statement.setStatus(ApplicationStatus.APPROVED);
+    public void applyOffer(LoanOfferDto dto){
+        UUID statementId = dto.getStatementId();
+        Statement statement = statementService.getStatement(statementId);
 
-        List<StatusHistory> history = statement.getStatusHistory();
-        if (history == null)
-            history = new ArrayList<StatusHistory>();
-        history.add(StatusHistory.builder()
-                .status(ApplicationStatus.APPROVED)
-                .changeType(ChangeType.AUTOMATIC)
-                .time(LocalDateTime.now()).build());
-        statement.setStatusHistory(history);
-
+        statementService.checkStatus(statement, ApplicationStatus.PREAPPROVAL);
+        statementService.changeStatus(statement, ApplicationStatus.APPROVED);
         statement.setAppliedOffer(offerMapper.dtoToAppliedOffer(dto));
-        // сохраняем изменения
-        statementRepository.save(statement);
-        logger.debug("applyOffer, save statement");
+        statementService.saveStatement(statement);
+
+        String email = statement.getClientId().getEmail();
+        String text = "Ваша заявка предварительно одобрена, завершите оформление";
+        EmailMessage emailMessage = EmailMessage.builder()
+                .address(email)
+                .theme(Topic.FINISH_REGISTRATION)
+                .statementId(statementId)
+                .text(text).build();
+        producerService.sendMessage(Topic.FINISH_REGISTRATION.getDescription(), emailMessage);
     }
 
-    public void calculateCredit(FinishRegistrationRequestDto dto, UUID statementId) throws DbException{
-        // поиск statement
-        Optional<Statement> optionalStatement = statementRepository.findById(statementId);
-        if (optionalStatement.isEmpty()){
-            logger.debug("calculateCredit, statement DbException");
-            throw new DbException("Ресурс с данным id не сущетсвует.", HttpStatus.BAD_REQUEST);
-        }
-        logger.debug("calculateCredit, find statement");
-        Statement statement = optionalStatement.get();
-        ApplicationStatus status = statement.getStatus();
-        if (status != ApplicationStatus.APPROVED){
-            logger.debug("calculateCredit, statement.status DbException");
-            throw  new DbException("В заявке неподходящий статус.", HttpStatus.BAD_REQUEST);
-        }
+    public void calculateCredit(FinishRegistrationRequestDto dto, UUID statementId){
+
+        Statement statement = statementService.getStatement(statementId);
+        statementService.checkStatus(statement, ApplicationStatus.APPROVED);
+
         // заполнение client
         Client client = statement.getClientId();
         client.setGender(dto.getGender());
@@ -159,7 +145,7 @@ public class DealService {
                 .isInsuranceEnabled(offer.getIsInsuranceEnabled())
                 .isSalaryClient(offer.getIsSalaryClient()).build();
 
-        CreditDto creditDto = calculatorClient.requestCalc(scoringDataDto);
+        CreditDto creditDto = calculatorClient.requestCalc(scoringDataDto, statementId);
 
         // сохранение credit
         Credit credit = creditMapper.dtoToCredit(creditDto);
@@ -169,16 +155,23 @@ public class DealService {
         logger.debug("calculateCredit, save credit");
 
         // сохранение statement
-        List<StatusHistory> history = statement.getStatusHistory();
-        history.add(StatusHistory.builder()
-                .status(ApplicationStatus.CC_APPROVED)
-                .changeType(ChangeType.AUTOMATIC)
-                .time(LocalDateTime.now()).build());
-        statement.setStatusHistory(history);
-        statement.setStatus(ApplicationStatus.CC_APPROVED);
         statement.setCreditId(credit);
-        statementRepository.save(statement);
+        statementService.changeStatus(statement, ApplicationStatus.CC_APPROVED);
+        statementService.saveStatement(statement);
+
         logger.debug("calculateCredit, save statement");
+
+        String email = statement.getClientId().getEmail();
+        String text = String.format(
+                "Кредит одобрен.\n Перейдите по ссылке для создания документов: %s/%s/send",
+                this.url, statementId.toString()
+        );
+        EmailMessage emailMessage = EmailMessage.builder()
+                .address(email)
+                .theme(Topic.CREATE_DOCUMENTS)
+                .statementId(statementId)
+                .text(text).build();
+        producerService.sendMessage(Topic.CREATE_DOCUMENTS.getDescription(), emailMessage);
     }
 
 
