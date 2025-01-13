00be377a5b114c3dffc4b802b1c4ec1cd0386a6e
V-inTim <vinichuk.timofey@mail.ru>
2025-01-13 22:02:06 +0300

    Add SesCodeDto.

--- deal/src/main/java/com/example/deal/controller/DocumentController.java
+++ deal/src/main/java/com/example/deal/controller/DocumentController.java
@@ -1,11 +1,15 @@
 package com.example.deal.controller;
 
+import com.example.deal.dto.SesCodeDto;
 import com.example.deal.service.DocumentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +21,8 @@ import java.util.UUID;
 @RequestMapping("/deal/document")
 public class DocumentController {
     private final DocumentService documentService;
+
+    private static final Logger logger = LoggerFactory.getLogger(DocumentController.class);
 
     @Autowired
     public DocumentController(DocumentService documentService) {
@@ -34,7 +40,9 @@ public class DocumentController {
     })
     @PostMapping(value = "/{statementId}/send")
     public ResponseEntity<Void> sendDocuments(@PathVariable UUID statementId){
+        logger.debug("request /{statementId}/send start");
         this.documentService.sendDocuments(statementId);
+        logger.debug("request /{statementId}/send start");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -63,7 +71,8 @@ public class DocumentController {
                     schema = @Schema(type = "object", example = "{\"field\":\"error\"}"))),
     })
     @PostMapping(value = "/{statementId}/code")
-    public ResponseEntity<Void> verifyCode(@PathVariable UUID statementId, @RequestHeader("ses-code") String sesCode){
+    public ResponseEntity<Void> verifyCode(@PathVariable UUID statementId, @RequestBody @Valid SesCodeDto dto){
+        String sesCode = dto.getSesCode();
         this.documentService.verifyCode(statementId, sesCode);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
