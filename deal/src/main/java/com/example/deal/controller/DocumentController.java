3869bc00f2d82ca70f9714a445deedde91ecdfb3
V-inTim <vinichuk.timofey@mail.ru>
2025-01-14 14:45:52 +0300

    Add and change logs.

--- deal/src/main/java/com/example/deal/controller/DocumentController.java
+++ deal/src/main/java/com/example/deal/controller/DocumentController.java
@@ -41,8 +41,10 @@ public class DocumentController {
     @PostMapping(value = "/{statementId}/send")
     public ResponseEntity<Void> sendDocuments(@PathVariable UUID statementId){
         logger.debug("request /{statementId}/send start");
+
         this.documentService.sendDocuments(statementId);
-        logger.debug("request /{statementId}/send start");
+
+        logger.debug("request /{statementId}/send finish");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -57,7 +59,11 @@ public class DocumentController {
     })
     @PostMapping(value = "/{statementId}/sign")
     public ResponseEntity<Void> signDocuments(@PathVariable UUID statementId){
+        logger.debug("request /{statementId}/sign start");
+
         this.documentService.signDocuments(statementId);
+
+        logger.debug("request /{statementId}/sign finish");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
@@ -73,7 +79,11 @@ public class DocumentController {
     @PostMapping(value = "/{statementId}/code")
     public ResponseEntity<Void> verifyCode(@PathVariable UUID statementId, @RequestBody @Valid SesCodeDto dto){
         String sesCode = dto.getSesCode();
+        logger.debug("request /{statementId}/code start");
+
         this.documentService.verifyCode(statementId, sesCode);
+
+        logger.debug("request /{statementId}/code finish");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
