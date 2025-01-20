0f551e527d242c76abe9e6cb999111c720736090
V-inTim <vinichuk.timofey@mail.ru>
2025-01-20 15:09:32 +0300

    Change controllers.

--- gateway/src/main/java/com/example/gateway/controller/DealController.java
+++ gateway/src/main/java/com/example/gateway/controller/DealController.java
@@ -1,11 +1,10 @@
 package com.example.gateway.controller;
 
-import com.example.gateway.client.DealClient;
 import com.example.gateway.dto.FinishRegistrationRequestDto;
 import com.example.gateway.dto.SesCodeDto;
+import com.example.gateway.service.DealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,35 +13,36 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/deal")
 public class DealController {
-    private final DealClient dealClient;
+    private final DealService dealService;
+
 
     @Autowired
-    public DealController(DealClient dealClient) {
-        this.dealClient = dealClient;
+    public DealController(DealService dealService) {
+        this.dealService = dealService;
     }
 
     @PostMapping("/calculate/{statementId}")
     public ResponseEntity<Void> calculateCredit(@PathVariable UUID statementId,
                                                 @RequestBody FinishRegistrationRequestDto dto){
-        dealClient.requestCalculateCredit(dto, statementId);
+        dealService.calculateCredit(statementId, dto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/document/{statementId}/send")
     public ResponseEntity<Void> sendDocuments(@PathVariable UUID statementId){
-        dealClient.requestDocumentsSend(statementId);
+        dealService.sendDocuments(statementId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/document/{statementId}/sign")
     public ResponseEntity<Void> signDocuments(@PathVariable UUID statementId){
-        dealClient.requestDocumentsSign(statementId);
+        dealService.signDocuments(statementId);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PostMapping("/document/{statementId}/code")
     public ResponseEntity<Void> verifySESCode(@PathVariable UUID statementId, @RequestBody SesCodeDto dto){
-        dealClient.requestDocumentsCode(statementId, dto);
+        dealService.documentCode(statementId, dto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
--- gateway/src/main/java/com/example/gateway/controller/StatementController.java
+++ gateway/src/main/java/com/example/gateway/controller/StatementController.java
@@ -1,35 +1,38 @@
 package com.example.gateway.controller;
 
-import com.example.gateway.client.StatementClient;
 import com.example.gateway.dto.LoanOfferDto;
 import com.example.gateway.dto.LoanStatementRequestDto;
+import com.example.gateway.service.StatementService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/statement")
 public class StatementController {
-    private final StatementClient statementClient;
+    private final StatementService statementService;
 
     @Autowired
-    public StatementController(StatementClient statementClient) {
-        this.statementClient = statementClient;
+    public StatementController(StatementService statementService) {
+        this.statementService = statementService;
     }
+
 
     @PostMapping("")
     public ResponseEntity<List<LoanOfferDto>> createStatement(@RequestBody LoanStatementRequestDto dto){
-        List<LoanOfferDto> offers = statementClient.requestCreateStatement(dto);
+        List<LoanOfferDto> offers = statementService.createStatement(dto);
         return new ResponseEntity<>(offers, HttpStatus.CREATED);
     }
 
     @PostMapping("/offer")
     public ResponseEntity<Void> applyOffer(@RequestBody LoanOfferDto dto){
-        statementClient.requestApplyOffer(dto);
+        statementService.applyOffer(dto);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
