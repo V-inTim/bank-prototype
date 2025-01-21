77128bb91656f2202be329cb6d6e996147fbba28
V-inTim <vinichuk.timofey@mail.ru>
2025-01-21 20:08:46 +0300

    Add swagger logic.

--- deal/src/main/java/com/example/deal/controller/AdminController.java
+++ deal/src/main/java/com/example/deal/controller/AdminController.java
@@ -2,6 +2,12 @@ package com.example.deal.controller;
 
 import com.example.deal.dto.StatementDto;
 import com.example.deal.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,17 +26,45 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @Operation(summary = "Изменить статус", description = "Изменить статус statement")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Void.class))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "object", example = "{\"field\":\"error\"}"))),
+    })
     @PutMapping("/statement/{statementId}/status")
     public ResponseEntity<Void> changeStatus(@PathVariable UUID statementId){
         adminService.changeStatus(statementId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Запрос statement", description = "Запрос statement по id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = StatementDto.class))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "object", example = "{\"field\":\"error\"}"))),
+    })
     @GetMapping("/statement/{statementId}")
     public ResponseEntity<StatementDto> getStatement(@PathVariable UUID statementId){
         StatementDto dto = adminService.getStatement(statementId);
         return new ResponseEntity<>(dto, HttpStatus.OK);
     }
+
+    @Operation(summary = "Запрос на все statement", description = "Запрос на все statement")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = StatementDto.class)))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "object", example = "{\"field\":\"error\"}"))),
+    })
     @GetMapping("/statement")
     public ResponseEntity<List<StatementDto>> getAllStatements(){
         List<StatementDto> dtoList = adminService.getAllStatements();
--- deal/src/main/java/com/example/deal/dto/StatementDto.java
+++ deal/src/main/java/com/example/deal/dto/StatementDto.java
@@ -3,6 +3,7 @@ package com.example.deal.dto;
 import com.example.deal.entity.AppliedOffer;
 import com.example.deal.entity.StatusHistory;
 import com.example.deal.type.ApplicationStatus;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,19 +18,28 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class StatementDto {
+    @Schema(description = "Клиент")
     private ClientDto client;
 
+    @Schema(description = "Кредит")
     private CreditDto credit;
 
+    @Schema(description = "Статус заявки")
     private ApplicationStatus status;
+
+    @Schema(description = "Дата создания")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
     private LocalDateTime creationDate;
 
+    @Schema(description = "Предложение")
     private AppliedOffer appliedOffer;
 
+    @Schema(description = "Дата подписания")
     private LocalDateTime signDate;
 
+    @Schema(description = "Код подтверждения")
     private String sesCode;
 
+    @Schema(description = "История статусов")
     private List<StatusHistory> statusHistory;
 }
