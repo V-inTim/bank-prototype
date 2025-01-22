4717ba0ec50a83b80dc863c3f65ae9a3e75f0178
V-inTim <vinichuk.timofey@mail.ru>
2025-01-22 18:08:59 +0300

    Add logs.

--- gateway/src/main/java/com/example/gateway/controller/StatementController.java
+++ gateway/src/main/java/com/example/gateway/controller/StatementController.java
@@ -3,6 +3,14 @@ package com.example.gateway.controller;
 import com.example.gateway.dto.LoanOfferDto;
 import com.example.gateway.dto.LoanStatementRequestDto;
 import com.example.gateway.service.StatementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +25,48 @@ import java.util.List;
 @RequestMapping("/statement")
 public class StatementController {
     private final StatementService statementService;
+    private static final Logger logger = LoggerFactory.getLogger(StatementController.class);
 
     @Autowired
     public StatementController(StatementService statementService) {
         this.statementService = statementService;
     }
 
-
+    @Operation(summary = "Создать statement", description = "Создать, сохранить statement")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "202", content = @Content(
+                    mediaType = "application/json",
+                    array = @ArraySchema(schema = @Schema(implementation = LoanOfferDto.class)))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "object", example = "{\"field\":\"error\"}"))),
+    })
     @PostMapping("")
     public ResponseEntity<List<LoanOfferDto>> createStatement(@RequestBody LoanStatementRequestDto dto){
+        logger.info("request /statement start");
+
         List<LoanOfferDto> offers = statementService.createStatement(dto);
+
+        logger.info("request /statement finish");
         return new ResponseEntity<>(offers, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Установить offer", description = "Сохранить выбранный offer")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Void.class))),
+            @ApiResponse(responseCode = "400", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(type = "object", example = "{\"field\":\"error\"}"))),
+    })
     @PostMapping("/offer")
     public ResponseEntity<Void> applyOffer(@RequestBody LoanOfferDto dto){
+        logger.info("request /statement/offer start");
+
         statementService.applyOffer(dto);
+
+        logger.info("request /statement/offer finish");
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
